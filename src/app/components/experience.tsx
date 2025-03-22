5d7830d4881f06b0f66f71a22e5baa3e85f91e6a
Ronald Almeida <ronaldmateus785@gmail.com>
2025-03-22 11:54:30 -0300

    Include logos in work experience

--- src/app/components/experience.tsx
+++ src/app/components/experience.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 export default function Experience() {
     return (
         <section id="experience" className="max-w-7xl m-auto p-4">
@@ -5,12 +7,23 @@ export default function Experience() {
 
             <div className="grid grid-cols-1 gap-y-4 max-w-3xl m-auto">
                 <div className="p-6 rounded-lg shadow-lg border-2 border-purple-600">
-                    <h3 className="text-xl font-semibold">
-                        Desenvolvedor Full Stack
-                    </h3>
-                    <p className="text-gray-400">
-                        ERA | Maio 2024 - Atualmente
-                    </p>
+                    <div className="flex gap-6">
+                        <Image
+                            src={"/assets/images/logos/era.png"}
+                            width={50}
+                            height={50}
+                            alt="Era"
+                            className="rounded-lg"
+                        />
+                        <div>
+                            <h3 className="text-xl font-semibold">
+                                Desenvolvedor Full Stack
+                            </h3>
+                            <p className="text-gray-400">
+                                ERA | Maio 2024 - Atualmente
+                            </p>
+                        </div>
+                    </div>
 
                     <ul className="mt-4 list-disc list-inside space-y-2 text-gray-300">
                         <li>
@@ -22,48 +35,10 @@ export default function Experience() {
                             performance.
                         </li>
                         <li>Interfaces responsivas e acessíveis.</li>
-                        <li>
-                            Modelagem e otimização de bancos de dados (MySQL e
-                            MongoDB).
-                        </li>
-                        <li>Testes unitários e de integração com Jest.</li>
+                        <li>Modelagem e otimização de bancos de dados.</li>
+                        <li>Testes automatizados e integração contínua.</li>
                         <li>
                             Refatoração de código legado para maior eficiência.
-                        </li>
-                    </ul>
-                </div>
-            </div>
-
-            <div className="mt-8 grid grid-cols-1 gap-8 gap-y-4 max-w-3xl m-auto">
-                <div className="p-6 rounded-lg shadow-lg border-2 border-purple-600">
-                    <h3 className="text-xl font-semibold">
-                        Desenvolvedor Full Stack
-                    </h3>
-                    <p className="text-gray-400">
-                        UC2B | Junho 2021 - Maio 2024
-                    </p>
-
-                    <ul className="mt-4 list-disc list-inside space-y-2 text-gray-300">
-                        <li>
-                            Desenvolvimento de APIs RESTful eficientes com
-                            Node.js e Express.
-                        </li>
-                        <li>Manutenção e escalabilidade de microsserviços.</li>
-                        <li>Criação de interfaces otimizadas.</li>
-                        <li>
-                            Otimização de bancos de dados MySQL, PostgreSQL e
-                            MongoDB.
-                        </li>
-                        <li>
-                            Testes automatizados e integração contínua (Jest,
-                            CI/CD).
-                        </li>
-                        <li>
-                            Refatoração de código e melhoria de performance.
-                        </li>
-                        <li>
-                            Implementação segura de autenticação e autorização
-                            (JWT).
                         </li>
                         <li>Colaboração com equipes multidisciplinares.</li>
                     </ul>
@@ -72,10 +47,58 @@ export default function Experience() {
 
             <div className="mt-8 grid grid-cols-1 gap-8 gap-y-4 max-w-3xl m-auto">
                 <div className="p-6 rounded-lg shadow-lg border-2 border-purple-600">
-                    <h3 className="text-xl font-semibold">
-                        Desenvolvedor Front-end
-                    </h3>
-                    <p className="text-gray-400">Freelancer | 2020</p>
+                    <div className="flex gap-6">
+                        <Image
+                            src={"/assets/images/logos/uc2b.svg"}
+                            width={50}
+                            height={50}
+                            alt="UC2B"
+                            className="rounded-lg"
+                        />
+                        <div>
+                            <h3 className="text-xl font-semibold">
+                                Desenvolvedor Full Stack
+                            </h3>
+                            <p className="text-gray-400">
+                                UC2B | Junho 2021 - Maio 2024
+                            </p>
+                        </div>
+                    </div>
+
+                    <ul className="mt-4 list-disc list-inside space-y-2 text-gray-300">
+                        <li>Desenvolvimento de APIs RESTful.</li>
+                        <li>Manutenção e escalabilidade de microsserviços.</li>
+                        <li>Criação de interfaces otimizadas.</li>
+                        <li>Otimização de bancos de dados MySQL, MongoDB.</li>
+                        <li>Testes unitários e de integração.</li>
+                        <li>
+                            Refatoração de código e melhoria de performance.
+                        </li>
+                        <li>
+                            Implementação segura de autenticação e autorização
+                            (JWT).
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+            <div className="mt-8 grid grid-cols-1 gap-8 gap-y-4 max-w-3xl m-auto">
+                <div className="p-6 rounded-lg shadow-lg border-2 border-purple-600">
+                    <div className="flex gap-6">
+                        <Image
+                            src={"/assets/images/logos/freelancer.svg"}
+                            width={50}
+                            height={50}
+                            alt="Freelancer"
+                            className="rounded-lg"
+                        />
+                        <div>
+                            <h3 className="text-xl font-semibold">
+                                Desenvolvedor Front-end
+                            </h3>
+                            <p className="text-gray-400">Freelancer | 2020</p>
+                        </div>
+                    </div>
 
                     <ul className="mt-4 list-disc list-inside space-y-2 text-gray-300">
                         <li>
