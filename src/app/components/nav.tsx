9a01b4f914c5385b84ca8fb5767a7852f083fc3d
Ronald Almeida <ronaldmateus785@gmail.com>
2025-03-26 12:48:47 -0300

    Update nav

--- src/app/components/nav.tsx
+++ src/app/components/nav.tsx
@@ -1,46 +1,85 @@
+import { useState } from "react";
 import Link from "next/link";
-import { IoMenu } from "react-icons/io5";
+import { IoClose, IoMenu } from "react-icons/io5";
+
+const navLinks = [
+    { label: "Sobre", href: "#about" },
+    { label: "Skills", href: "#skills" },
+    { label: "Experiência", href: "#experience" },
+    { label: "Projetos", href: "#projects" },
+    { label: "Contato", href: "#contact" },
+];
 
 export default function Nav() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
     return (
-        <section className="fixed w-full top-0 backdrop-blur-xl shadow-custom z-1">
-            <div className="max-w-7xl m-auto p-4 h-24 flex justify-between items-center text-center text-lg">
-                <ul className="flex justify-center items-center">
-                    <li className="cursor-pointer title">
-                        <Link href={"#home"} className="font-extrabold">
-                            Ronald Almeida
-                        </Link>
-                    </li>
-                </ul>
-                <ul className="hidden md:flex items-center">
-                    <li className="cursor-pointer mr-4">
-                        <Link href={"#about"} className="font-bold">
-                            Sobre
-                        </Link>
-                    </li>
-                    <li className="cursor-pointer mr-4">
-                        <Link href={"#experience"} className="font-bold">
-                            Experiência
-                        </Link>
-                    </li>
-                    <li className="cursor-pointer mr-4">
-                        <Link href={"#projects"} className="font-bold">
-                            Projetos
-                        </Link>
-                    </li>
-                    <li className="cursor-pointer mr-4">
-                        <Link href={"#skills"} className="font-bold">
-                            Skills
-                        </Link>
-                    </li>
-                    <li className="cursor-pointer mr-4">
-                        <Link href={"#contact"} className="font-bold">
-                            Contato
-                        </Link>
-                    </li>
-                </ul>
-                <IoMenu size={30} className="md:hidden cursor-pointer" />
-            </div>
-        </section>
+        <>
+            {/* Seção para telas grandes */}
+            <section className="fixed w-full top-0 backdrop-blur-xl shadow-custom z-1">
+                <div className="max-w-7xl m-auto p-4 h-24 flex justify-between items-center text-center text-lg">
+                    <ul className="flex justify-center items-center">
+                        <li className="cursor-pointer title">
+                            <Link href={"#home"} className="font-extrabold">
+                                Ronald Almeida
+                            </Link>
+                        </li>
+                    </ul>
+                    <ul className="hidden md:flex items-center">
+                        {navLinks.map(({ label, href }) => (
+                            <li className="cursor-pointer mr-4" key={href}>
+                                <Link href={href} className="font-bold">
+                                    {label}
+                                </Link>
+                            </li>
+                        ))}
+                    </ul>
+                    <IoMenu
+                        size={30}
+                        className="md:hidden cursor-pointer"
+                        onClick={toggleMenu}
+                    />
+                </div>
+            </section>
+
+            {/* Seção para telas pequenas */}
+            <section
+                className={`fixed w-full h-full top-0 backdrop-blur-3xl shadow-custom z-1 transition-all duration-300 ${
+                    isMenuOpen ? "block" : "hidden"
+                }`}
+            >
+                <div className="max-w-7xl m-auto p-4 h-full flex flex-col justify-center items-center gap-8 text-center text-lg relative">
+                    <ul className="flex justify-center items-center">
+                        <li className="cursor-pointer title">
+                            <Link href={"#home"} className="font-extrabold">
+                                Ronald Almeida
+                            </Link>
+                        </li>
+                    </ul>
+                    <ul className="flex flex-col items-center gap-8">
+                        {navLinks.map(({ label, href }) => (
+                            <li
+                                className="cursor-pointer mr-4"
+                                onClick={toggleMenu}
+                                key={href}
+                            >
+                                <Link href={href} className="font-bold">
+                                    {label}
+                                </Link>
+                            </li>
+                        ))}
+                    </ul>
+                    <IoClose
+                        size={30}
+                        className="absolute top-8 right-4 cursor-pointer"
+                        onClick={toggleMenu}
+                    />
+                </div>
+            </section>
+        </>
     );
 }
