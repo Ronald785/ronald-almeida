7d22c34f2acf3455e59c6bd207730302791f6f1f
Ronald Almeida <ronaldmateus785@gmail.com>
2025-03-26 13:01:14 -0300

    Update home

--- src/app/components/home.tsx
+++ src/app/components/home.tsx
@@ -7,20 +7,23 @@ export default function Home() {
     return (
         <section
             id="home"
-            className="mt-24 max-w-7xl m-auto bg-1 grid grid-cols-1 md:grid-cols-2 gap-1 stars p-4"
+            className="mt-36 max-w-3xl m-auto bg-1 grid grid-cols-1 md:grid-cols-1 gap-1 stars p-4 mb-12"
         >
-            <div className="flex flex-col justify-center items-center md:items-start">
+            <div className="flex flex-col justify-center items-center ">
                 <h1 className="text-3xl md:text-4xl font-extrabold">
                     Ronald Almeida
                 </h1>
                 <h3 className="text-2xl mt-4">Desenvolvedor Full Stack</h3>
-                <p className="mt-4 text-center md:text-start">
+                <p className="mt-4 text-center">
                     Criando sonhos com as tecnologias mais modernas.
                     Especializado em construir sistemas, sites e aplicativos
                     usando React, Node.js, TypeScript e Javascript.
                 </p>
                 <div className="mt-4 flex gap-3">
-                    <Link href={"https://github.com/Ronald785"} target="_blank">
+                    <Link
+                        href={"/assets/archives/CV - Ronald Almeida.pdf"}
+                        target="_blank"
+                    >
                         <div className="border-2 border-chart-4 rounded-lg p-2 opacity-80 hover:opacity-90 flex justify-center items-center">
                             <TbFileCv
                                 size={20}
@@ -28,7 +31,10 @@ export default function Home() {
                             />
                         </div>
                     </Link>
-                    <Link href={"https://github.com/Ronald785"} target="_blank">
+                    <Link
+                        href={"https://www.linkedin.com/in/ronald785/"}
+                        target="_blank"
+                    >
                         <div className="border-2 border-chart-4 rounded-lg p-2 opacity-80 hover:opacity-90 flex justify-center items-center">
                             <FaLinkedinIn
                                 size={20}
@@ -46,7 +52,6 @@ export default function Home() {
                     </Link>
                 </div>
             </div>
-            <div className="div"></div>
         </section>
     );
 }
